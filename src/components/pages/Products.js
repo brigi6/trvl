defc63696cf08124e716aabba4eefc966c289012
brigi <mannadsgn@gmail.com>
2022-01-18 16:59:04 +0100

    final

---
+++ src/components/pages/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Products = () => {
+    return (
+        <div>
+         <h1 className='products'>PRODUCTS</h1>
+        </div>
+    )
+}
+
+export default Products
+
